b02f2c8429f3cb057d31faec0fead8428bf0c462
stmu <904574519@qq.com>
2018-07-29 14:23:41 +0800

    feat: 虚线

--- demo/demo.js
+++ demo/demo.js
@@ -1,21 +1,20 @@
 
-// const canvas = new ZE.Canvas('container', {
-//   width: 800,
-//   height: 600,
-//   style: {
-//     strokeStyle: 'black'
-//   }
-// });
+const canvas = new ZE.Canvas('container', {
+  width: 800,
+  height: 600,
+  style: {
+    strokeStyle: 'black'
+  }
+});
 
-// canvas.addShape('line', {
-//   attrs: {
-//     x1: 0,
-//     y1: 400,
-//     x2: 800,
-//     y2: 400,
-//     dash: 10
-//   }
-// });
+canvas.addShape('line', {
+  attrs: {
+    x1: 0,
+    y1: 10,
+    x2: 1000,
+    y2: 500,
+    dash: 5
+  }
+});
 
-// canvas.draw();
-
+canvas.draw();
